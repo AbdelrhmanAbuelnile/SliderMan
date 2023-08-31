a34cc2bfdf63f64b01304fd433ae19bc55c1dfd7
Abdelrahman Hany <98924552+AbdelrhmanAbuelnile@users.noreply.github.com>
2023-08-31 11:13:03 +0300

    added component for each slide

--- src/App.jsx
+++ src/App.jsx
@@ -1,145 +1,134 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/effect-cube';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/effect-cube";
+import "swiper/css/pagination";
 
-import './App.css';
+// import components
+import RightSlide1 from "./components/RightSlider/RightSLide1";
+import RightSlide2 from "./components/RightSlider/RightSLide2";
+import RightSlide3 from "./components/RightSlider/RightSLide3";
+import RightSlide4 from "./components/RightSlider/RightSLide4";
+import RightSlide5 from "./components/RightSlider/RightSLide5";
+import RightSlide6 from "./components/RightSlider/RightSLide6";
+import RightSlide7 from "./components/RightSlider/RightSLide7";
+import RightSlide8 from "./components/RightSlider/RightSLide8";
+import RightSlide9 from "./components/RightSlider/RightSLide9";
+
+import LeftSlide1 from "./components/LeftSlider/LeftSLide1";
+import LeftSlide2 from "./components/LeftSlider/LeftSLide2";
+import LeftSlide3 from "./components/LeftSlider/LeftSLide3";
+import LeftSlide4 from "./components/LeftSlider/LeftSLide4";
+import LeftSlide5 from "./components/LeftSlider/LeftSLide5";
+import LeftSlide6 from "./components/LeftSlider/LeftSLide6";
+import LeftSlide7 from "./components/LeftSlider/LeftSLide7";
+import LeftSlide8 from "./components/LeftSlider/LeftSLide8";
+import LeftSlide9 from "./components/LeftSlider/LeftSLide9";
+
+import "./App.css";
 
 // import required modules
-import { EffectCube, Pagination, Controller, Autoplay } from 'swiper/modules';
+import { EffectCube, Pagination, Controller, Autoplay } from "swiper/modules";
 
 export default function App() {
   const pagination = {
     clickable: true,
   };
+  const speed = 1000
   const [controlledSwiper, setControlledSwiper] = useState(null);
   return (
-    <div className='container'>
+    <div className="container">
       <Swiper
-        effect={'cube'}
-        cubeEffect={{
-          shadow: true,
-          slideShadows: true,
-          shadowOffset: 20,
-          shadowScale: 0.94,
-        }}
-        direction={'vertical'}
+        effect={"cube"}
+        direction={"vertical"}
         pagination={pagination}
-        modules={[EffectCube, Pagination,Controller]}
-        onSlide = {console.log('slide')}
+        modules={[EffectCube, Pagination, Controller]}
+        onSlide={console.log("slide")}
         className="mySwiper"
         controller={{ control: controlledSwiper }}
+        speed={speed}
       >
         <SwiperSlide>
-          <div className='content__main'>
-              <p>Enter Rat Park at your own risk. Lose the cage. Find yourself. An L.A. power couple
-                orders an A.I. sex droid to escape their marital prison. What happens to them could
-                happen to you. With his fourth novel, Adam Novak shows us how it takes a village of
-                automatons to augment your humanity, feed your deceptive compulsive sexual
-                addiction, and forget why you ever got married in the first place.
-              </p>
-              <div className="frame">
-                <a href="https://a.co/d/0p21mCR" target="_blank">
-                    <button className="custom-btn btn-12"><span>Hurry!</span><span>Buy
-                            Now</span></button>
-                </a>
-              </div>
-              <br/>
-              <div className="quotes">
-                <div className="quotes">
-                    <p className="quote"></p>
-                    <p className="author"></p>
-                    <p className="affiliation"></p>
-                </div>
-              </div>
-          </div>
-          <h3 class="content__index">01</h3>
-        </SwiperSlide>
-        
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+          <RightSlide1 />
         </SwiperSlide>
 
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+          <RightSlide2 />
         </SwiperSlide>
 
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+          <RightSlide3 />
         </SwiperSlide>
 
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+          <RightSlide4 />
         </SwiperSlide>
 
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+          <RightSlide5 />
         </SwiperSlide>
 
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+          <RightSlide6 />
         </SwiperSlide>
 
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+          <RightSlide7 />
         </SwiperSlide>
 
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+          <RightSlide8 />
         </SwiperSlide>
 
+        <SwiperSlide>
+          <RightSlide9 />
+        </SwiperSlide>
       </Swiper>
 
       <Swiper
-        effect={'cube'}
-        cubeEffect={{
-          shadow: true,
-          slideShadows: true,
-          shadowOffset: 20,
-          shadowScale: 0.94,
-        }}
-        direction={'vertical'}
+        effect={"cube"}
+        direction={"vertical"}
         modules={[EffectCube, Pagination, Controller, Autoplay]}
         reverseDirection={true}
         onSwiper={setControlledSwiper}
-        onSlide = {console.log('slide')}
+        onSlide={console.log("slide")}
         autoplay={{
           delay: 150000,
           disableOnInteraction: true,
         }}
+        speed={speed}
         className="mySwiper left"
       >
-        <div className='content'>
+        <div className="content">
           <SwiperSlide>
-            <img src="/assets/adam_novak.jpg" />
+            <LeftSlide1 />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="/assets/excerpts.png" />
+            <LeftSlide2 />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="/assets/freaks_of_the_industry.jpg" />
+            <LeftSlide3 />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="/assets/podcast.png" />
+            <LeftSlide4 />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="/assets/rat_park_audio_book.jpg" />
+            <LeftSlide5 />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="/assets/rat_park_shirt.png" />
+            <LeftSlide6 />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="/assets/rat_park.jpg" />
+            <LeftSlide7 />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="/assets/take_fountain.jpg" />
+            <LeftSlide8 />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="/assets/the_non_pro.jpg" />
+            <LeftSlide9 />
           </SwiperSlide>
         </div>
       </Swiper>
---
+++ src/components/RightSlider/RightSLide2.jsx
@@ -0,0 +1,39 @@
+export default function RightSlide2() {
+  return (
+    <>
+      <div className="content__main">
+        <p>
+          In the spirit of Elmore Leonard's Get Shorty and the HBO series
+          Entourage, Adam Novak's crime novel, The Non-Pro, skewers the
+          Hollywood scene like a picador to a bull. When movie industry insider
+          Jerry Makos dies under mysterious circumstances, his brother Josh
+          seeks the killer while trying not to get sucked into the abyss of the
+          movie industry. Who would want his brother dead? That was the problem.
+          . .the list of social climbers and celebrity parasites seemed to be
+          endless. Just when it seems like Josh will never find the killer, the
+          planets align for a perfect third act.
+        </p>
+        <div className="frame">
+          <a
+            href="https://www.simonandschuster.com/books/The-Non-Pro/Adam-Novak/9781627934473"
+            target="_blank"
+          >
+            <button className="custom-btn btn-12">
+              <span>Hurry!</span>
+              <span>Buy Now</span>
+            </button>
+          </a>
+        </div>
+        <br />
+        <div className="quotes">
+          <div className="quotes">
+            <p className="quote"></p>
+            <p className="author"></p>
+            <p className="affiliation"></p>
+          </div>
+        </div>
+      </div>
+      <h3 className="content__index">02</h3>
+    </>
+  );
+}
